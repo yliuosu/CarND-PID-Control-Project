64dc5a7e4a503d78ee46a32c70cc5fa7c7a5b081
Yang Liu <yliuosu@gmail.com>
2017-07-24 23:23:24 -0500

    feat: Implement the initial functions of the PID class.

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,16 +6,42 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+	
+	p_error = 0.0;
+	i_error = 0.0;
+	d_error = 0.0;
+	
+	Kp = 1.0;
+	Ki = 1.0;
+	Kd = 1.0;
+	
+	prev_cte = 0.0;
+	
+}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+	prev_cte = 0.0;
+	total_cte = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+	
+	d_error = cte - prev_cte;
+	p_error = cte;
+	i_error += cte;
+	prev_ct = cte;
+	
 }
 
 double PID::TotalError() {
+	
+	
+	return 
 }
 
