2addc475014894aa1997d673d66685560d25455b
Yang <yliuosu@gmail.com>
2017-07-27 21:43:32 -0500

    feat: Prune the variables and functions and add corresponding comments.

--- src/PID.h
+++ src/PID.h
@@ -24,28 +24,40 @@ public:
   
   double prev_cte;
   double total_cte;
-  
+   
+   // time steps
    int msgcount;
    
+   // whem twiddle the parameter
+   // flag to show whether it is the first run
    bool first_run;
    bool twiddle;
+   
+   // record the smallest sum square cte 
+   // of multiple runs
    double best_error;
    
+   // stack to record previous operation on the parameters
    std::stack<Operations> stack_operations;
    
    // which parameter to adjust
-   // 0  dp_p;
-   // 1  dp_d;
-   // 2  dp_i;
+   // 0  dp_p to adjust  Kp;
+   // 1  dp_d to adjust  Kd;
+   // 2  dp_i to adjust  Ki;
    int adjust_index;
    double dp[3];
    
+   // record the kp and kd which delivers the best performance
    double best_kp;
    double best_kd;
    
+   // maximum speed is the real speed X 10 and take the interger part
    int  max_speed;
+   // number of maximum speed 
    int nummaxspeed;
    
+   // record the maximum speed and number 
+   // in multiple runs 
    int best_max_speed;
    int best_nummaxspeed;
    
@@ -72,8 +84,8 @@ public:
   /*
   * Calculate the total PID error.
   */
-  double TotalError();
-  
+
+ // change the range of the adjustment array 
   void MulitpleDP(double factor);
   
   void AdjustParams();
@@ -82,11 +94,16 @@ public:
   
   int CheckOperations() ;
   
+  // check if this run has better results
   bool Better();
   
   void UpdateSpeed(double speed) ;
   
   void Twiddle(int nStep);
+  
+  void SetTwiddleFlag(bool flag);
+  
+  bool  GetTwiddleFlag();
   
   void DisplayPerformanceComparison(int stage);
   
